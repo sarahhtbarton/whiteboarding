ae9a986e8e2ff49411a80ed5bb48237bd3a8e346
Sarah <sarahhtbarton@gmail.com>
2021-07-01 19:38:23 +0000

    the question

--- add_to_zero.py
+++ add_to_zero.py
@@ -1,2 +1,33 @@
-https://fellowship.hackbrightacademy.com/materials/challenges/add-to-zero/index.html
+# https://fellowship.hackbrightacademy.com/materials/challenges/add-to-zero/index.html
 
+"""Given list of ints, return True if any two nums in list sum to 0.
+
+    >>> add_to_zero([])
+    False
+
+    >>> add_to_zero([1])
+    False
+
+    >>> add_to_zero([1, 2, 3])
+    False
+
+    >>> add_to_zero([1, 2, 3, -2])
+    True
+
+Given the wording of our problem, a zero in the list will always
+make this true, since "any two numbers" could include that same
+zero for both numbers, and they sum to zero:
+
+    >>> add_to_zero([0, 1, 2])
+    True
+"""
+
+
+def add_to_zero(nums):
+    """Given list of ints, return True if any two nums sum to 0."""
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. NOTHING ESCAPES YOU!\n")
