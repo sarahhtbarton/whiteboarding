39d6b30d794d6c4ac36a9d265531b9ce38aad491
Sarah <sarahhtbarton@gmail.com>
2021-07-02 15:40:18 +0000

    Anagram of Palindrome - Solved

--- anagramofpalindrome.py
+++ anagramofpalindrome.py
@@ -1,3 +1,5 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/anagram-of-palindrome/index.html
+
 """Is the word an anagram of a palindrome?
 
 A palindrome is a word that reads the same forward and backwards
@@ -29,6 +31,24 @@ Examples::
 
 def is_anagram_of_palindrome(word):
     """Is the word an anagram of a palindrome?"""
+
+    char_count_dict = {}
+
+    for char in word:
+        if char_count_dict.get(char) == None:
+            char_count_dict[char] = 1
+        else:
+            char_count_dict[char] += 1
+    
+    # It's a palindrome if the number of odd-counts is either 0 or 1
+
+    odd_count = 0
+
+    for value in char_count_dict.values():
+        if value % 2 != 0:
+            odd_count += 1
+    
+    return odd_count == 0 or odd_count == 1
 
 
 if __name__ == '__main__':
