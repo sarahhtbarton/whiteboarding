73fa3ad18b15476b2d61e434e35d67c017fe8457
Sarah <sarahhtbarton@gmail.com>
2021-07-22 22:12:50 +0000

    Pig Latin - Problem

---
+++ piglatin.py
@@ -0,0 +1,34 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/pig-latin/index.html
+
+"""Turn a phrase into Pig Latin.
+
+This takes a space-separated phrase and returns it in Pig Latin.
+
+Rules:
+
+1. If the word begins with a consonant (not a, e, i, o, u),
+   move the first letter to the end and add 'ay'
+
+2. If the word begins with a vowel, add 'yay' to the end
+
+For example:
+
+    >>> pig_latin('hello awesome programmer')
+    'ellohay awesomeyay rogrammerpay'
+
+"""
+
+def pig_latin(phrase):
+    """Turn a phrase into pig latin.
+
+    There will be no uppercase letters or punctuation in the phrase.
+
+        >>> pig_latin('hello awesome programmer')
+        'ellohay awesomeyay rogrammerpay'
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. REATGAY OBJAY!\n")
