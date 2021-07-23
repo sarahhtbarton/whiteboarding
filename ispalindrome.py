e7c91755c647d0cb72b3c045647e7bd265eac08c
Sarah <sarahhtbarton@gmail.com>
2021-07-23 16:08:23 +0000

    Is Palindrome - Problem

---
+++ ispalindrome.py
@@ -0,0 +1,32 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/is-palindrome/index.html
+
+"""Is this word a palindrome?
+
+    >>> is_palindrome("a")
+    True
+
+    >>> is_palindrome("noon")
+    True
+
+    >>> is_palindrome("racecar")
+    True
+
+    >>> is_palindrome("porcupine")
+    False
+
+Treat spaces and uppercase letters normally:
+
+    >>> is_palindrome("Racecar")
+    False
+"""
+
+
+def is_palindrome(word):
+    """Return True/False if this word is a palindrome."""
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YAY!")
