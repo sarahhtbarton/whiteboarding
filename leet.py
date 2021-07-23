548f519e64200f76d30f2dd94318181475ec318a
Sarah <sarahhtbarton@gmail.com>
2021-07-23 17:50:26 +0000

    Leet Speak - Problem

---
+++ leet.py
@@ -0,0 +1,24 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/leet-speak/index.html
+
+"""Translate phrase to "leet-speak".
+
+For example::
+
+    >>> translate_leet("Hi Balloonicorn")
+    'Hi B@1100nic0rn'
+
+    >>> translate_leet("Hackbright is the Shizzle")
+    'H@ckbrigh7 i5 7h3 5hizz13'
+
+"""
+
+
+def translate_leet(phrase):
+    """Translates input into "leet-speak"."""
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. AE3S0M3!\n")
