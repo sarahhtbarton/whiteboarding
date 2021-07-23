00043945c9dd4eabab190dc40a68357630853530
Sarah <sarahhtbarton@gmail.com>
2021-07-23 18:10:23 +0000

    Longest Word - Problem

---
+++ longest-word.py
@@ -0,0 +1,23 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/longest-word/index.html
+
+"""Return longest word in list of words.
+
+For example::
+
+    >>> find_longest_word(["hi", "hello"])
+    5
+
+    >>> find_longest_word(["Balloonicorn", "Hackbright"])
+    12
+
+"""
+
+
+def find_longest_word(words):
+    """Return longest word in list of words."""
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE THE MAX!\n")
