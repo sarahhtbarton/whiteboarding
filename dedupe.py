25c69e66ae4adcc83fda3dbc3affb78fbfc6a36f
Sarah <sarahhtbarton@gmail.com>
2021-07-25 19:08:50 +0000

    Remove Duplicates - Problem

---
+++ dedupe.py
@@ -0,0 +1,45 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/remove-duplicates/index.html
+
+"""Return new list from items with duplicates removed.
+
+For example::
+
+    >>> deduped([1, 1, 1])
+    [1]
+
+Keep items in the order where they first appeared::
+
+    >>> deduped([1, 2, 1, 1, 3])
+    [1, 2, 3]
+
+A list with no duplicates would return the same::
+
+    >>> deduped([1, 2, 3])
+    [1, 2, 3]
+
+This should return a *new* list, not mutate the existing
+list::
+
+    >>> a = [1, 2, 3]
+    >>> b = deduped(a)
+    >>> a == b
+    True
+
+    >>> a is b
+    False
+
+An empty list should return an empty list::
+
+    >>> deduped([])
+    []
+
+"""
+
+
+def deduped(items):
+    """Return new list from items with duplicates removed."""
+    
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE NO DUPE!\n")
