08b4bb638afd0cddb46cfb4e46bef91df75624c8
Sarah <sarahhtbarton@gmail.com>
2021-07-25 20:31:27 +0000

    Snake Case to Camel Case - Problem

---
+++ case.py
@@ -0,0 +1,20 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/snake-to-camel/index.html
+
+"""Given a variable name in snake_case, return camelCase of name.
+
+For example::
+
+    >>> snake_to_camel("hi_balloonicorn")
+    'hiBalloonicorn'
+
+"""
+
+
+def snake_to_camel(variable_name):
+    """Given a variable name in snake_case, return camelCase of name."""
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. HOORAY!\n")
