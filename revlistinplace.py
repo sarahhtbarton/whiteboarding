774233bffc1e45e4da08fd73673ae5663f2f9422
Sarah <sarahhtbarton@gmail.com>
2021-07-25 19:20:55 +0000

    Reverse List in Place - Problem

---
+++ revlistinplace.py
@@ -0,0 +1,40 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/rev-list-in-place/index.html
+
+"""Reverse list in place.
+
+You cannot do this with reversed(), .reverse(), or list slice
+assignment!
+
+    >>> lst = []
+    >>> rev_list_in_place(lst)
+    >>> lst
+    []
+
+    >>> lst = ['a']
+    >>> rev_list_in_place(lst)
+    >>> lst
+    ['a']
+
+    >>> lst = [1, 2, 3]
+    >>> rev_list_in_place(lst)
+    >>> lst
+    [3, 2, 1]
+
+    >>> lst = [1, 2, 3, 4]
+    >>> rev_list_in_place(lst)
+    >>> lst
+    [4, 3, 2, 1]
+"""
+
+
+def rev_list_in_place(lst):
+    """Reverse list in place.
+
+    You cannot do this with reversed(), .reverse(), or list slice
+    assignment!
+    """
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE THE BEST!\n")
