079db9e32eb554da9fd6ee35482e856e9d55129b
Sarah <sarahhtbarton@gmail.com>
2021-07-25 20:01:30 +0000

    Show Even Numbers - Problem

---
+++ showevens.py
@@ -0,0 +1,26 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/show-evens/index.html
+
+"""Given list of ints, return list of *indices* of even numbers in list.
+
+For example::
+
+    >>> show_evens([])
+    []
+
+    >>> show_evens([2])
+    [0]
+
+    >>> show_evens([1, 2, 3, 4])
+    [1, 3]
+
+"""
+
+
+def show_evens(nums):
+    """Given list of ints, return list of *indices* of even numbers in list."""
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. EVENLY HANDLED!\n")
