4627db50724dfd058f0e9c3406234b9082e8ffcd
Sarah <sarahhtbarton@gmail.com>
2021-07-25 18:42:45 +0000

    Remove Duplicates - Problem

---
+++ remove_duplicates.py
@@ -0,0 +1,28 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/remove-duplicates/index.html
+
+""" Remove duplicates in a list
+
+For example::
+
+    >>> remove_duplicates([6, 9, 7, 9, 2, 6, 0])
+    [6, 9, 7, 2, 0]
+
+    >>> remove_duplicates([])
+    []
+
+    >>> remove_duplicates([6, 9, 7])
+    [6, 9, 7]
+
+"""
+
+
+def remove_duplicates(items):
+    """Remove duplicates in the list items and return that list."""
+    
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. GREAT WORK!\n")
+
+
+
