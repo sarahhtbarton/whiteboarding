4bb3ac7174f5d93335b3ca4c8c8a28ca17612b21
Sarah <sarahhtbarton@gmail.com>
2021-07-25 19:58:14 +0000

    Reverse a String - Problem

---
+++ revstring.py
@@ -0,0 +1,29 @@
+# https://fellowship.hackbrightacademy.com/materials/challenges/rev-string/index.html
+
+"""Reverse a string.
+
+For example::
+
+    >>> rev_string("")
+    ''
+
+    >>> rev_string("a")
+    'a'
+
+    >>> rev_string("porcupine")
+    'enipucrop'
+
+"""
+
+
+def rev_string(astring):
+    """Return reverse of string.
+
+    You may NOT use the reversed() function!
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. !KROW DOOG\n")
