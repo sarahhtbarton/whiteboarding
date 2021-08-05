be921c82fb5303bc87eb4b751846cd0fa7581e50
Sarah <sarahhtbarton@gmail.com>
2021-08-05 20:10:39 +0000

    Snake Case to Camel Case - Solved

--- case.py
+++ case.py
@@ -13,6 +13,18 @@ For example::
 def snake_to_camel(variable_name):
     """Given a variable name in snake_case, return camelCase of name."""
 
+    underscore_index = 0
+
+    for i in range(len(variable_name)):
+        if variable_name[i] == '_':
+            underscore_index = i
+    
+    camel_case = variable_name[:underscore_index]
+    camel_case += variable_name[underscore_index+1:].title()
+
+    return camel_case
+
+
 
 if __name__ == '__main__':
     import doctest
